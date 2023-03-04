65b8d69c29b0a14290c849ef0330ed9843f47488
thecoderpunk <ravisingh02082001@gmail.com>
2023-03-04 18:09:10 +0530

    versal uplaod

--- themarket/themarket/settings.py
+++ themarket/themarket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ty&dp(v4nvto+^-1ros*^_rtd_-rw%wzs+o=ek^su+zx5v6yzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
