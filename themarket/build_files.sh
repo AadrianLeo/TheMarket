f785870254b452f3342ef30b5c9f203f38cad7f9
thecoderpunk <ravisingh02082001@gmail.com>
2023-03-04 17:57:08 +0530

    versal uplaod

--- themarket/build_files.sh
+++ themarket/build_files.sh
@@ -1,3 +1,3 @@
-# build_files.sh
-pip install -r requirements.txt
-python3 manage.py collectstatic
+python3.8 -m pip install -r requirements.txt
+python3.8 manage.py collectstatic --noinput --clear
+
