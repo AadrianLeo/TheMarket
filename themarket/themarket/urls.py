f98486022039e1f91934f870748fb21029568d3e
thecoderpunk <ravisingh02082001@gmail.com>
2023-03-04 17:47:49 +0530

    versal uplaod

--- themarket/themarket/urls.py
+++ themarket/themarket/urls.py
@@ -25,3 +25,6 @@ urlpatterns = [
     path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
